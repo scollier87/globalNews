3f9122cf163ca212ad7e75ff0f1e8bc28006ceea
SeanyC <sean.collier928@yahoo.com>
2024-02-01 20:00:14 -0800

    styling NewsFeed and added img

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useFetchArticles from "../useFetchArticles/useFetchArticles";
+import './NewsFeed.css';
 
 const NewsFeed = ({ apiKey }) => {
     const { articles, isLoading, error } = useFetchArticles(apiKey, 'technology', '');
@@ -9,10 +10,12 @@ const NewsFeed = ({ apiKey }) => {
             {isLoading && <p>Loading...</p>}
             {error && <p>Error: {error}</p>}
             {articles.map((article, index) => (
-                <article key={index}>
-                    <h2>{article.title}</h2>
-                    <p>{article.description}</p>
-                    {/* {further details...} */}
+                <article key={index} className="article">
+                    <img src={article.urlToImage} alt={article.title}/>
+                    <div className="article-content">
+                        <h2 className="article-title">{article.title}</h2>
+                        <p className="article-description">{article.description}</p>
+                    </div>
                 </article>
             ))}
         </div>
