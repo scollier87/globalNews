0c01254b868352ed1d90f3b4dddb50fe261d6d54
SeanyC <sean.collier928@yahoo.com>
2024-02-02 14:51:26 -0800

    implemented infinite scrolling

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { timeSince } from "../../utilities/dateUtils";
 import useFetchArticles from "../useFetchArticles/useFetchArticles";
 import './NewsFeed.css';
 
 const NewsFeed = ({ apiKey }) => {
+    const [displayArticles, setDisplayArticles] = useState([]);
+    const [articlesToDisplay, setArticlesToDisplay] = useState(9);
     const { articles, isLoading, error } = useFetchArticles(apiKey, 'technology', '');
+
+    useEffect(() => {
+        setDisplayArticles(articles.slice(0, articlesToDisplay));
+    }, [articlesToDisplay, articles]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+            setArticlesToDisplay(prevCount => prevCount + 9);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     return (
         <div>
             {isLoading && <p>Loading...</p>}
             {error && <p>Error: {error}</p>}
             <div className="news-feed">
-                {articles.map((article, index) => (
+                {displayArticles.map((article, index) => (
                     <div key={index} className="news-card">
                     <img src={article.urlToImage} alt={article.title} />
                     <div className="news-card-body">
@@ -29,3 +45,6 @@ const NewsFeed = ({ apiKey }) => {
 };
 
 export default NewsFeed
+
+//limit viewport height so not all articles visible
+//track scroll position in regards to loading more articles window.innerheight window.offsetheight
